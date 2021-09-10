721b77c22f9809e4b8efb868f888974143c66125
priya6100 <priyaranjan@grapsnextsocial.com>
2021-09-10 14:39:12 +0530

    urlconfig

---
+++ src/urlConfig.js
@@ -0,0 +1,11 @@
+const baseUrl = 
+ location.hostname === "localhost"
+  ? "http://localhost:7000" 
+  : "https://soulbyindian-backend.herokuapp.com";
+
+
+export const api = `${baseUrl}/api`;
+export const generatePublicUrl = (fileName) =>{
+
+    return `${baseUrl}/public/${fileName}`;
+}
